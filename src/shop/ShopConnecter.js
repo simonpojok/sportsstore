9c36231cb49cf3ec96dd58b6ff2149e93f4e2edd
simonojok19 <simonojok19@gmail.com>
2020-06-16 16:55:00 +0300

    created the connector

---
+++ src/shop/ShopConnecter.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { loadData } from "../data/ActionCreators";
+import { DataTypes } from "../data/Types";
+import { Shop } from "./Shop";
+
+const mapStateToProps = (dataStore) => ({
+  ...dataStore,
+});
+
+const mapDispatchToProps = {
+  loadData,
+};
+
+const filterProducts = (products = [], category) =>
+  !category || category === "All"
+    ? products
+    : products.filter(
+        (product) => product.toLowerCase() === category.toLowerCase()
+      );
+
+export const ShopConnector = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(
+  class extends React.Component {
+      componentDidMount() {
+          this.props.loadData(DataTypes.CATEGORIES);
+          this.props.loadData(DataTypes.PRODUCTS)
+      }
+    render() {
+      return (
+        <Switch>
+          <Route
+            path="/shop/products/:category?"
+            render={(routeProps) => (
+              <Shop
+                {...this.props}
+                {...routeProps}
+                products={filterProducts(
+                  this.props.products,
+                  routeProps.match.params.category
+                )}
+              />
+            )}
+          />
+          <Redirect to="/shop/products" />
+        </Switch>
+      );
+    }
+  }
+);
