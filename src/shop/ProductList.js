f69a5ec637036ade2d1e94023f4db2faf5ecaa5e
simonojok19 <simonojok19@gmail.com>
2020-06-17 08:48:35 +0300

    added add to cart button

--- src/shop/ProductList.js
+++ src/shop/ProductList.js
@@ -13,7 +13,15 @@ export class ProductList extends React.Component {
             ${product.price.toFixed(2)}
           </span>
         </h4>
-        <div className="card-text bg-white p-1">{product.description}</div>
+        <div className="card-text bg-white p-1">
+          {product.description}
+          <button
+            className="btn btn-success btn-sm float-right"
+            onClick={() => this.props.addToCart(p)}
+          >
+            Add To Cart
+          </button>
+        </div>
       </div>
     ));
   }
