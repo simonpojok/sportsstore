d2ce5b8f788855b2a716ec0e5768de50ea115b5a
simonojok19 <simonojok19@gmail.com>
2020-06-17 17:47:35 +0300

    formated the data for inspection

--- src/data/ActionCreators.js
+++ src/data/ActionCreators.js
@@ -3,17 +3,19 @@ import { ActionTypes } from "./Types";
 import { RestDataSource } from "./RestDataSource";
 const dataSource = new RestDataSource();
 
-export const loadData = (dataType, params) => ({
-  type: ActionTypes.DATA_LOAD,
-  payload: dataSource
-    .GetData(dataType, params)
-    .then((response) => ({
+export const loadData = (dataType, params) => {
+  const data = {
+    type: ActionTypes.DATA_LOAD,
+    payload: dataSource.GetData(dataType, params).then((response) => ({
       dataType,
       data: response.data,
       total: Number(response.headers["x-total-count"]),
       params,
     })),
-});
+  };
+  console.log(data);
+  return data;
+};
 
 // payload: {
 //     dataType: "load",
