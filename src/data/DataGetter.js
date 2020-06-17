f4ca800ec5071a2bfb49b6ba77f84d4cef42b850
simonojok19 <simonojok19@gmail.com>
2020-06-17 14:59:57 +0300

    added the get data method

---
+++ src/data/DataGetter.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { DataTypes } from "./Types";
+
+export class DataGetter extends React.Component {
+    componentDidMount = () => this.getData();
+    componentWillUpdate = () => this.getData();
+
+    getData = () => {
+        const dsData = this.props.products_params || { };
+        const rtData = {
+            _limit: this.props.pageSize || 5,
+            _sort: this.props.sortKey || "name",
+            _page: this.props.match.params.page || 1,
+            category_like: (this.props.match.params.category || "") === "all" ?
+            "" : this.props.match.params.category
+        }
+        if (Object.keys(rtData).find((key) => dsData[key] !== rtData[key])) {
+            this.props.loadData(DataTypes.PRODUCTS, rtData);
+        }
+    }
+    render() {
+    return <React.Fragment>{this.props.children}</React.Fragment>
+    }
+}
