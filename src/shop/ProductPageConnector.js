b10f159e57536e8d3ac6ceca3342cef4a604bc5d
simonojok19 <simonojok19@gmail.com>
2020-06-18 15:40:40 +0300

    review the codes

--- src/shop/ProductPageConnector.js
+++ src/shop/ProductPageConnector.js
@@ -1,28 +1,35 @@
-import React from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import { setPageSize, setSortProperty } from "../data/ActionCreators";
 
-const mapStateToProps = (dataStore) => dataStore;
-const mapDispatchToProps = {
-  setPageSize,
-  setSortProperty,
+const mapStateToProps = (dataStore, ownProps) => {
+  console.log(dataStore);
+  console.log(ownProps);
+  return dataStore;
+};
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    setPageSize,
+    setSortProperty,
+  };
 };
 
-const mergeProps = (dataStore, actionCreators, router) => ({
-  ...dataStore,
-  ...router,
-  ...actionCreators,
-  currentPage: Number(router.match.params.page),
-  pageCount: Math.ceil(
-    (dataStore.products_total || dataStore.pageSize || 5) /
-      (dataStore.pageSize || 5)
-  ),
-  navigateToPage: (page) =>
-    router.history.push(
-      `/shop/products/${router.match.params.category}/${page}`
+const mergeProps = (dataStore, actionCreators, router) => {
+  return {
+    ...dataStore,
+    ...router,
+    ...actionCreators,
+    currentPage: Number(router.match.params.page),
+    pageCount: Math.ceil(
+      (dataStore.products_total || dataStore.pageSize || 5) /
+        (dataStore.pageSize || 5)
     ),
-});
+    navigateToPage: (page) =>
+      router.history.push(
+        `/shop/products/${router.match.params.category}/${page}`
+      ),
+  };
+};
 
 export const ProductPageConnector = (PageComponent) =>
   withRouter(
