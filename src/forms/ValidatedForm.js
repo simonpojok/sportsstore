2a4ca2a130a21f2628f6f59afd0270c95c362529
simonojok19 <simonojok19@gmail.com>
2020-06-19 12:02:28 +0300

    added the codes

--- src/forms/ValidatedForm.js
+++ src/forms/ValidatedForm.js
@@ -1,3 +1,15 @@
 import React from "react";
 import { ValidationError } from "./ValidationError";
 import { GetMessages } from "./ValidationMessages"
+
+export class ValidatedForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            validationErrors: { }
+        };
+        this.formElements = { };
+    }
+
+    handle
+}
