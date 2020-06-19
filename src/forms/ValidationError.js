7915a559a4e1791f65ad405869b14ef6176ddb40
simonojok19 <simonojok19@gmail.com>
2020-06-19 15:28:34 +0300

    formated the codes

--- src/forms/ValidationError.js
+++ src/forms/ValidationError.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 export class ValidationError extends React.Component {
-    render() {
-        if (this.props.errors) {
-            return this.props.errors.map(error => (
-            <h6 className="text-danger" key={error}>{error}</h6>
-            ))
-        }
-        return null;
+  render() {
+    if (this.props.errors) {
+      return this.props.errors.map((error) => (
+        <h6 className="text-danger" key={error}>
+          {error}
+        </h6>
+      ));
     }
+    return null;
+  }
 }
