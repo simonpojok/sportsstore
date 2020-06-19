dc4ffc825e0c7c053c61120922726caed63870d6
simonojok19 <simonojok19@gmail.com>
2020-06-19 13:00:06 +0300

    done reviewing middleware

--- src/data/AsyncMiddleware.js
+++ src/data/AsyncMiddleware.js
@@ -1,13 +1,27 @@
- const isPromise = (payload) =>
+const isPromise = (payload) =>
   (typeof payload === "object" || typeof payload === "function") &&
   typeof payload.then === "function";
 
-export const asyncActions = ({getState, dispatch}) => (next) => (action) => {
+export const asyncActions = ({ getState, dispatch }) => (next) => (action) => {
   if (isPromise(action.payload)) {
     action.payload.then((result) => {
-      return next({ ...action, payload: result })
+      return next({ ...action, payload: result });
     });
   } else {
     next(action);
   }
 };
+
+export function asyncActionsDeport({ getState, dispatch }) {
+  return function (next) {
+    return function (action) {
+      if (isPromise(action.payload)) {
+        action.payload.then((result) => {
+          return next({ ...action, payload: result });
+        });
+      } else {
+        next(action);
+      }
+    };
+  };
+}
